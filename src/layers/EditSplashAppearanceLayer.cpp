c66c7b57054d190b097ee7c3182a8a7560c96dec
ShineUA <playimit@gmail.com>
2024-06-25 21:41:13 +0200

    made this in better way

--- src/layers/EditSplashAppearanceLayer.cpp
+++ src/layers/EditSplashAppearanceLayer.cpp
@@ -28,7 +28,10 @@ bool EditSplashAppearanceLayer::init() {
     this->setKeypadEnabled(true);
     this->setTouchEnabled(true);
     auto bg = CCSprite::create("game_bg_01_001.png");
-    bg->setPosition(bg->getContentSize() / 2);
+    bg->setScale(CCDirector::sharedDirector()->getWinSizeInPixels().width / bg->getTexture()->getContentSizeInPixels().width);
+    bg->setContentWidth(1024);
+    bg->setTextureRect(CCRect(0, 0, 1024, 512));
+    bg->setPosition((bg->getContentSize() / 2) * (CCDirector::sharedDirector()->getWinSizeInPixels().width / bg->getTexture()->getContentSizeInPixels().width));
     bg->setColor({0, 255, 255});
     bg->setID("bg-1");
     bg->runAction(CCRepeatForever::create(CCSequence::create(
@@ -40,52 +43,13 @@ bool EditSplashAppearanceLayer::init() {
         CCDelayTime::create(0.5),
         nullptr
     )));
-    auto bg2 = CCSprite::create("game_bg_01_001.png");
-    bg2->setPosition({(bg->getContentWidth() / 2) + bg->getContentWidth(), bg->getContentHeight() / 2});
-    bg2->setColor({0, 255, 255});
-    bg2->setID("bg-2");
-    bg2->runAction(CCRepeatForever::create(CCSequence::create(
-        CCTintTo::create(2.5, 255, 0, 255),
-        CCDelayTime::create(0.5),
-        CCTintTo::create(2.5, 255, 255, 0),
-        CCDelayTime::create(0.5),
-        CCTintTo::create(2.5, 0, 255, 255),
-        CCDelayTime::create(0.5),
+    bg->runAction(CCRepeatForever::create(CCSequence::create(
+        CCMoveBy::create(15, {-bg->getContentWidth() / 2 * (CCDirector::sharedDirector()->getWinSizeInPixels().width / bg->getTexture()->getContentSizeInPixels().width), 0}),
+        CCMoveBy::create(0, {bg->getContentWidth() / 2 * (CCDirector::sharedDirector()->getWinSizeInPixels().width / bg->getTexture()->getContentSizeInPixels().width), 0}),
         nullptr
     )));
-    auto bg3 = CCSprite::create("game_bg_01_001.png");
-    bg3->setPosition({(bg->getContentWidth() / 2) + bg->getContentWidth() * 2, bg->getContentHeight() / 2});
-    bg3->setColor({0, 255, 255});
-    bg3->setID("bg-3");
-    bg3->runAction(CCRepeatForever::create(CCSequence::create(
-        CCTintTo::create(2.5, 255, 0, 255),
-        CCDelayTime::create(0.5),
-        CCTintTo::create(2.5, 255, 255, 0),
-        CCDelayTime::create(0.5),
-        CCTintTo::create(2.5, 0, 255, 255),
-        CCDelayTime::create(0.5),
-        nullptr
-    )));
-    bg->runAction(CCSequence::create(
-        CCMoveBy::create(15, {-bg->getContentWidth(), 0}),
-        CCCallFuncN::create(bg, callfuncN_selector(EditSplashAppearanceLayer::bgLoop)),
-        nullptr
-    ));
-    bg2->runAction(CCSequence::create(
-        CCMoveBy::create(15 * 2, {-bg2->getContentWidth() * 2, 0}),
-        CCCallFuncN::create(bg2, callfuncN_selector(EditSplashAppearanceLayer::bgLoop)),
-        nullptr
-    ));
-    bg3->runAction(CCSequence::create(
-        CCMoveBy::create(15 * 3, {-bg2->getContentWidth() * 3, 0}),
-        CCCallFuncN::create(bg3, callfuncN_selector(EditSplashAppearanceLayer::bgLoop)),
-        nullptr
-    ));
-    
     
     this->addChild(bg);
-    this->addChild(bg2);
-    this->addChild(bg3);
     return true;
 }
 
